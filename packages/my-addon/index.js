8f0d637735ed9aa7b5d77f8ef156d0cef6eeb606
Julien Palmas <julien@crx.io>
2019-10-07 16:55:13 +0200

    add third-party-package

--- packages/my-addon/index.js
+++ packages/my-addon/index.js
@@ -1,4 +1,11 @@
+/* eslint-disable node/no-extraneous-require */
 'use strict';
+
+const Funnel = require("broccoli-funnel");
+const path = require("path");
+const mergeTrees = require('broccoli-merge-trees');
+const log = require('broccoli-stew').log;
+
 
 module.exports = {
   name: require('./package').name,
@@ -6,6 +13,17 @@ module.exports = {
   included(app) {
     this._super.included.apply(this, arguments);
 
-    app.import('vendor/third-party.js');
+    app.import('vendor/my-third-party-package/third-party.js');
+  },
+
+  treeForVendor(vendorTree) {
+    const assetDir = path.join(this.project.root + '/../../node_modules/my-third-party-package/src');
+
+    var thirdPartyTree = new Funnel(assetDir, {
+      files: ['third-party.js'],
+      destDir: 'my-third-party-package'
+    });
+
+    return mergeTrees([vendorTree, log(thirdPartyTree)]);
   }
 };
